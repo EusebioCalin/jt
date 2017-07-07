8ead7d71475863561228e97fb6f27d4988e96d13
Calin Cotet <eusebio.cotet@gmail.com>
2017-07-07 13:44:19 +0300

    modificat operatie

--- calc/src/impl/Divide.java
+++ calc/src/impl/Divide.java
@@ -7,7 +7,8 @@ public class Divide implements Operation{
 	@Override
 	public Double calc(Double a, Double b) {
 		if (b!=0)
-		return a/b;
+		return a/(a+b);
+			//return a/b;
 		else throw new IllegalArgumentException();
 	}
 
